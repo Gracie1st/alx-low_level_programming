0b5ea1187b50f188171460bc8c9ba55f1519b2d6
Gracie1st <daviesgracie7@gmail.com>
2022-09-23 14:41:58 -0700

    Task 0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,12 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+int _putchar(char c);
 #include <stdio.h>
 /**
  * void prototypes()
  * int prototypes()
  * char prototypes()
  */
-void reverse_array(int*a, int n);
+void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
 int_strcmp(char *s1, char *s2);
