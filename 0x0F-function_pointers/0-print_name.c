94d4fe800b5d60e15f2d619b921538bde09ea025
Gracie1st <daviesgracie7@gmail.com>
2022-10-14 08:48:56 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,14 +3,12 @@
 /**
  * print_name - prints a name
  * @name:name to print
- * @f: function which print name
+ * @f: function which
  *
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-
+	if (name != NULL && f != NULL)
 		f(name);
 }
