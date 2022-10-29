46f6d27b60815db93d59c2506a3fbefb6d1c9190
Gracie1st <daviesgracie7@gmail.com>
2022-10-29 01:32:20 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,9 +1,10 @@
 #include <stddef.h>
 #include "main.h"
 
-/*
+/**
  * check_valid_string - checks if a string has only 0's and 1's
  * @b: string to be checked
+ *
  * Return: 1 if string is valid, 0 otherwise
  */
 int check_valid_string(const char *b)
@@ -12,7 +13,7 @@ int check_valid_string(const char *b)
 		return (0);
 	while (*b)
 	{
-		if (*b != '1' && *b != '0'
+		if (*b != '1' && *b != '0')
 			return (0);
 		b++;
 	}
@@ -27,9 +28,7 @@ int check_valid_string(const char *b)
  *
  * Return: the converted value
  */
-
-nsigned int binary_to_uint(const char *b)
-
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int decimal = 0;
 	int str_len = 0, base = 1;
